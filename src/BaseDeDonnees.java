a02ded3c959af199b894c9be7a01210986c4a0bd
Silmetal <Silmetal@users.noreply.github.com>
2017-05-29 15:00:33 +0200

    Squelette javaDoc BDD

--- src/BaseDeDonnees.java
+++ src/BaseDeDonnees.java
@@ -2,32 +2,61 @@ package model;
 
 import java.sql.*;
 
+/**
+ *
+ */
 public BaseDeDonnees {
 	
+	/**
+	 *
+	 */
+	private Connection connection;
+	
+	/**
+	 *
+	 */
 	public BaseDeDonnees(String adresse, String nomUtili, String motDePasse){
 		
 	}
 	
+	/**
+	 *
+	 */
 	private boolean verifDriver() {
 		
 	}
 	
+	/**
+	 *
+	 */
 	private void connexion(String adresse, String motDePasse) {
 		
 	}
 	
+	/**
+	 *
+	 */
 	public Statement createStatement(){
 		
 	}
 	
+	/**
+	 *
+	 */
 	public void addNewID(){
 		
 	}
 	
+	/**
+	 *
+	 */
 	public void write(){
 		
 	}
 	
+	/**
+	 *
+	 
 	public void read(){
 		
 	}
