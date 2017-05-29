3524225e8af7a0687c1b2e64880ddb069d0f0709
Silmetal <Silmetal@users.noreply.github.com>
2017-05-29 15:33:09 +0200

    BDD javaDoc V0.3

--- src/BaseDeDonnees.java
+++ src/BaseDeDonnees.java
@@ -3,7 +3,16 @@ package model;
 import java.sql.*;
 
 /**
- *
+ * Cette classe établit une connexion à une base de données grâce à JDBC et les classes de java.sql.
+ * La classe crée un objet Connnection sur lequel on peut effectuer des requêtes grâce à l'objet Statement également crée et à la classe Requête.
+ * Exemple pour oracle : 
+ * /n BaseDeDonnees bdd = new BaseDeDonnees("jdbc:oracle:thin:@localhsot:1521:xe", "root", "root")
+ * "jdbc:oracle:thin:" indique que l'on se connect à une BDD oracle.
+ * /n "localhost" correspond au nom d'hôte
+ * /n "1521" correspond au port de la base
+ * /n "xe" correspond au SID.
+ * Les deux derniers champs sont, respectivement, le nom d'utilisateur et le mot de passe.
+ * 
  */
 public BaseDeDonnees {
 	
@@ -36,7 +45,7 @@ public BaseDeDonnees {
 	/**
 	 *
 	 */
-	public Statement createStatement(){
+	private Statement createStatement(){
 		
 	}
 	
