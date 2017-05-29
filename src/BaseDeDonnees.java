89bb183a975c6b6031c8ffb0ac50d4c6fbfec128
Silmetal <Silmetal@users.noreply.github.com>
2017-05-29 14:56:09 +0200

    Squelette BaseDeDonnées

---
+++ src/BaseDeDonnees.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.sql.*;
+
+public BaseDeDonnees {
+	
+	public BaseDeDonnees(String adresse, String nomUtili, String motDePasse){
+		
+	}
+	
+	private void verifDriver() {
+		
+	}
+	
+	private void connexion(String adresse, String motDePasse) {
+		
+	}
+	
+	public Statement createStatement(){
+		
+	}
+	
+	public void addNewID(){
+		
+	}
+	
+	public void write(){
+		
+	}
+	
+	public void read(){
+		
+	}
+	
+}
