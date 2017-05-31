f07036a94f541410ea44e489afc5acdf39e9eb03
arthurguyader <arthurguyader@gmail.com>
2017-05-31 09:24:25 +0200

    BaseDeDonneesTest
    Avancer mais passer les methodes privés au méthode public

--- src/test/BaseDeDonneesTest.java
+++ src/test/BaseDeDonneesTest.java
@@ -1,9 +1,9 @@
-package test;
-
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class BaseDeDonneesTest{
+
+	private BaseDeDonnees maBase;
 
 	public BaseDeDonneesTest(String name){
 		super(name);
@@ -11,14 +11,11 @@ public class BaseDeDonneesTest{
 
 
 	@Before()
-	protected void miseEnPlace(){
-		
+	protected void miseEnPlace(){		
 	}
-
 
 	@Test()
 	public void testVerifPilote(){
-
 		boolean test=true;
 		
 		try {
@@ -27,17 +24,54 @@ public class BaseDeDonneesTest{
             test=false;
         }
 
-        assertEquals(test,BaseDeDonnees.verifPilote());
+        Assert.assertEquals(test,BaseDeDonnees.verifPilote());
+	}
+	
+
+	@Test() // valeur local actuellement
+	public void testConnexion(){
+		boolean res
+
+		//Test de la connexion avec des valeurs valides
+
+		res = BaseDeDonnees.connexion("jdbc:oracle:thin:@localhost:1521:xe","root", "root"); 
+		Assert.assertTrue(res);
 
 
+		//Test de la connexion avec des valeurs erronées
+
+		//Adresse erronée
+
+		res = BaseDeDonnees.connexion("jdbc:oracle:thin:@localhost:1521:orcl","root", "root"); 
+		Assert.assertFalse(res);
+		//Nom Utilisateur erroné
+		res = BaseDeDonnees.connexion("jdbc:oracle:thin:@localhost:1521:xe","admin", "root"); 
+		Assert.assertFalse(res);
+		//Mot de Passe erroné
+		res = BaseDeDonnees.connexion("jdbc:oracle:thin:@localhost:1521:xe","root", "admin"); 
+		Assert.assertFalse(res);
 	}
 
 
+	@Test() // valeur local actuellement
+	public void testCreerRequete(){
+
+		// test avec l'attribut connexion qui existe
+
+		BaseDeDonnees maBase = new BaseDeDonnees("jdbc:oracle:thin:@localhost:1521:xe","root", "root");
+		Statement maRequete = maBase.creerRequete()
+
+		Assert.assertTrue(maRequete!=null);
+
+		// test sans l'attribut connexion
+
+		BaseDeDonnees maBase2 = new BaseDeDonnees("jdbc:oracle:thin:@localhost:1521:orcl","root", "root");
+		Statement maRequete2 = maBase.creerRequete()
+
+		Assert.assertTrue(maRequete2==null);
 
 
-
-
-
+	}
 
 
 
