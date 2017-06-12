6bef41f42434e14339028c340e51e2589c3e2997
Silmetal <Silmetal@users.noreply.github.com>
2017-06-12 13:34:30 +0200

    Coding v0.51

--- src/model/Requete.java
+++ src/model/Requete.java
@@ -14,42 +14,52 @@ public class Requete {
 	}
 	
 	public int MenuChoix() {
+		
+		return null;
 	
 	}
 	
 	public int ajouterTable() {
 		
+		return null;
 	}
 	
 	public int ajouterTuple() {
 		
+		return null;
 	}
 	
 	public int ajouterTrigger() {
 		
+		return null;
 	}
 	
 	public int ajouterVue() {
 		
+		return null;
 	}
 	
 	public int enleverTable() {
 		
+		return null;
 	}
 	
 	public int enleverTuple() {
-		
+		return null;
 	}
 	
 	public int enleverTrigger() {
 		
+		return null;
 	}
 	
 	public int enleverVue() {
 		
+		return null;
 	}
 	
 	public int manuel() {
 		
+		return null;
 	}
 }
