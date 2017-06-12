448d8999606b360525d875dc8ac0d78d75f50bd9
Silmetal <Silmetal@users.noreply.github.com>
2017-06-12 11:29:04 +0200

    Coding V0.6

---
+++ src/model/Requete.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.sql.*;
+
+/**
+ * 
+ */
+public class Requete {
+
+	private Statement state;
+	
+	public Requete(Statement state) {
+		
+	}
+	
+	public int MenuChoix() {
+	
+	}
+	
+	public int ajouterTable() {
+		
+	}
+	
+	public int ajouterTuple() {
+		
+	}
+	
+	public int ajouterTrigger() {
+		
+	}
+	
+	public int ajouterVue() {
+		
+	}
+	
+	public int enleverTable() {
+		
+	}
+	
+	public int enleverTuple() {
+		
+	}
+	
+	public int enleverTrigger() {
+		
+	}
+	
+	public int enleverVue() {
+		
+	}
+	
+	public int manuel() {
+		
+	}
+}
---
+++ src/model/Utilisateur.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.sql.*;
+
+/**
+ * 
+ */
+public class Utilisateur {
+	
+	private String id;
+	
+	public Utilisateur(String id) {
+		
+	}
+	
+	public void connect() {
+	
+	}
+	
+	public void disconnect(int index) {
+		
+	}
+	
+	public void nouvelleRequete() {
+		
+	}
+	
+	public String toString() {
+		
+	}
+}
