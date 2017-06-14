d1b6a026a2d6ddaa9010002d0b3f2e86a3e37e39
Silmetal <Silmetal@users.noreply.github.com>
2017-06-14 10:32:34 +0200

    aze

--- src/model/BaseDeDonnees.java
+++ src/model/BaseDeDonnees.java
@@ -179,9 +179,9 @@ public class BaseDeDonnees {
 		
 		try {
 			creerLocalSuperUser = connexion.prepareStatement("CREATE USER ?@'localhost' IDENTIFIED BY ?; GRANT ALL PRIVILEGES ON *.* TO ?@'localhost' WITH GRANT OPTION;");
-			creerLocalSuperUser.setString(1,"'"+nouvIdenti+"'");
-			creerLocalSuperUser.setString(2,"'"+nouvMDP+"'");
-			creerLocalSuperUser.setString(3,"'"+nouvIdenti+"'");
+			creerLocalSuperUser.setString(1,nouvIdenti);
+			creerLocalSuperUser.setString(2,nouvMDP);
+			creerLocalSuperUser.setString(3,nouvIdenti);
 		}
 		catch(SQLException se) {
 			throw se;
@@ -189,9 +189,9 @@ public class BaseDeDonnees {
 		
 		try{
 			creerGlobalSuperUser = connexion.prepareStatement("CREATE USER ?@'%' IDENTIFIED BY ?; GRANT ALL PRIVILEGES ON *.* TO ?@'%' WITH GRANT OPTION;");
-			creerGlobalSuperUser.setString(1,"'"+nouvIdenti+"'");
-			creerGlobalSuperUser.setString(2,"'"+nouvMDP+"'");
-			creerGlobalSuperUser.setString(3,"'"+nouvIdenti+"'");
+			creerGlobalSuperUser.setString(1,nouvIdenti);
+			creerGlobalSuperUser.setString(2,nouvMDP);
+			creerGlobalSuperUser.setString(3,nouvIdenti);
 		}
 		catch(SQLException se) {
 			throw se;
@@ -199,10 +199,10 @@ public class BaseDeDonnees {
 		
 		try{
 			creerLocalUser = connexion.prepareStatement("CREATE USER ?@'localhost' IDENTIFIED BY ?; GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON ?.* TO ?@'localhost';");
-			creerLocalUser.setString(1,"'"+nouvIdenti+"'");
-			creerLocalUser.setString(2,"'"+nouvMDP+"'");
-			creerLocalUser.setString(3,"'"+connexion.getMetaData().getDatabaseProductName()+"'");
-			creerLocalUser.setString(4,"'"+nouvIdenti+"'");
+			creerLocalUser.setString(1,nouvIdenti);
+			creerLocalUser.setString(2,nouvMDP);
+			creerLocalUser.setString(3,connexion.getMetaData().getDatabaseProductName());
+			creerLocalUser.setString(4,nouvIdenti);
 		}
 		catch(SQLException se) {
 			throw se;
@@ -210,12 +210,12 @@ public class BaseDeDonnees {
 		
 		try {
 			creerGlobalUser = connexion.prepareStatement("CREATE USER ?@? IDENTIFIED BY ?; GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON ?.* TO ?@?;");
-			creerGlobalUser.setString(1,"'"+nouvIdenti+"'");
-			creerGlobalUser.setString(2,"'"+connexion.getMetaData().getURL()+"'");
-			creerGlobalUser.setString(3,"'"+nouvMDP);
-			creerGlobalUser.setString(4,"'"+connexion.getMetaData().getDatabaseProductName()+"'");
-			creerGlobalUser.setString(5,"'"+nouvIdenti+"'");
-			creerGlobalUser.setString(6,"'"+connexion.getMetaData().getURL()+"'");
+			creerGlobalUser.setString(1,nouvIdenti);
+			creerGlobalUser.setString(2,connexion.getMetaData().getURL());
+			creerGlobalUser.setString(3,nouvMDP);
+			creerGlobalUser.setString(4,connexion.getMetaData().getDatabaseProductName());
+			creerGlobalUser.setString(5,nouvIdenti);
+			creerGlobalUser.setString(6,connexion.getMetaData().getURL());
 		}
 		catch(SQLException se) {
 			throw se;
