97b6b866ee6d643baf29cb1e710967fa167b2820
Silmetal <Silmetal@users.noreply.github.com>
2017-06-14 10:00:04 +0200

    azeaze

--- src/model/BaseDeDonnees.java
+++ src/model/BaseDeDonnees.java
@@ -179,9 +179,9 @@ public class BaseDeDonnees {
 		
 		try {
 			creerLocalSuperUser = connexion.prepareStatement("CREATE USER '?'@'localhost' IDENTIFIED BY '?'; GRANT ALL PRIVILEGES ON *.* TO '?'@'localhost' WITH GRANT OPTION;");
-			creerLocalSuperUser.setString(1,nouvIdenti);
-			creerLocalSuperUser.setString(2,nouvMDP);
-			creerLocalSuperUser.setString(3,nouvIdenti);
+			creerLocalSuperUser.setString(0,nouvIdenti);
+			creerLocalSuperUser.setString(1,nouvMDP);
+			creerLocalSuperUser.setString(2,nouvIdenti);
 		}
 		catch(SQLException se) {
 			throw se;
@@ -189,9 +189,9 @@ public class BaseDeDonnees {
 		
 		try{
 			creerGlobalSuperUser = connexion.prepareStatement("CREATE USER '?'@'%' IDENTIFIED BY '?'; GRANT ALL PRIVILEGES ON *.* TO '?'@'%' WITH GRANT OPTION;");
-			creerGlobalSuperUser.setString(1,nouvIdenti);
-			creerGlobalSuperUser.setString(2,nouvMDP);
-			creerGlobalSuperUser.setString(3,nouvIdenti);
+			creerGlobalSuperUser.setString(0,nouvIdenti);
+			creerGlobalSuperUser.setString(1,nouvMDP);
+			creerGlobalSuperUser.setString(2,nouvIdenti);
 		}
 		catch(SQLException se) {
 			throw se;
@@ -199,10 +199,10 @@ public class BaseDeDonnees {
 		
 		try{
 			creerLocalUser = connexion.prepareStatement("CREATE USER '?'@'localhost' IDENTIFIED BY '?'; GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON ?.* TO '?'@'localhost';");
-			creerLocalUser.setString(1,nouvIdenti);
-			creerLocalUser.setString(2,nouvMDP);
-			creerLocalUser.setString(3,connexion.getMetaData().getDatabaseProductName());
-			creerLocalUser.setString(4,nouvIdenti);
+			creerLocalUser.setString(0,nouvIdenti);
+			creerLocalUser.setString(1,nouvMDP);
+			creerLocalUser.setString(2,connexion.getMetaData().getDatabaseProductName());
+			creerLocalUser.setString(3,nouvIdenti);
 		}
 		catch(SQLException se) {
 			throw se;
