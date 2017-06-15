21218768aff2f3ecd1a7b04e43f00f10b97c5dbc
arthurguyader <arthurguyader@gmail.com>
2017-06-15 08:51:19 +0200

    javadoc utilisateur

--- src/model/Utilisateur.java
+++ src/model/Utilisateur.java
@@ -32,14 +32,19 @@ public class Utilisateur {
 
 	/** Permet de se connecter a une base de donnée oracle ou mySQL.
 	* @param url L'adresse de la base de donnée
-	* @param user Un nom d'utilisateur ()
-	* @param 
+	* @param user Un nom d'utilisateur (Attention selon l'endroit où vous vous connectez 
+	* , il est nécéssaire d'avoir des droits particuliers, prenez donc en compte se détail)
+	* @param password Le mot de passe de l'utilisateur pour accéder à la base de donnée
 	*/
 	public void connect(String url,String user, String password) {
 		lesBasesDeDonnees.add(new BaseDeDonnees(url,user,password));
 		selection = lesBasesDeDonnees.size();
 	}
 	
+
+	/** Permet de se deconnecter de la base de donnée sélectionné 
+	*
+	*/
 	public void disconnect() throws NullPointerException {
 		if(this.selection!=-1 && this.selection>=0 && this.selection<lesBasesDeDonnees.size()){
 			lesBasesDeDonnees.get(this.selection).deconnexion();
@@ -50,8 +55,15 @@ public class Utilisateur {
 			throw new NullPointerException("Attention selection incorect");
 		}
 	}
+
+	/** Fait appel a la base de donnée séléctionnée pour créer une nouvelle instance
+	* de la classe requete.
+	*
+	*/
 	
 	public void nouvelleRequete() {
+		
+		Requete nouvelleRequete = new Requete(this.lesBasesDeDonnees.get(this.selection));
 		
 	}
 	
