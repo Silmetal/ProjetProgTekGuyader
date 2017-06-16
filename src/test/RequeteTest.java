9af5a9eda58a80fbbe2b3c4df3a20460707aaab5
Silmetal <Silmetal@users.noreply.github.com>
2017-06-16 12:43:30 +0200

    sfqsf

---
+++ src/test/RequeteTest.java
@@ -0,0 +1,81 @@
+package test;
+import model.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.sql.*;
+
+
+public class BaseDeDonneesTest{
+
+	private BaseDeDonnees maBase;
+
+	public BaseDeDonneesTest(){
+		
+	}
+
+
+	@Before()
+	public void miseEnPlace(){		
+	}
+
+	@Test()
+	public void testConstructeur(){
+		boolean test=true;
+		BaseDeDonnees maBase2 = null;
+		BaseDeDonnees maBase = null;
+		
+		try{
+			maBase = new BaseDeDonnees("jdbc:mysql://arthurguyader.ddns.net:3306/","admin", "admin");
+			assertNotNull(maBase.getConnection());
+		}
+		catch(ClassNotFoundException ce){
+			assertFalse(true);
+		}
+		catch(SQLException se){
+			assertFalse(true);
+		}
+		catch(Exception e){
+			assertFalse(true);
+		}
+		finally {
+			maBase.deconnexion();
+		}
+
+
+    	try{
+    		maBase2 = new BaseDeDonnees("jdbc:mysql://arthurguyader.ddns.net:3306/","qfqsfn", "admin");
+    	}
+    	catch(ClassNotFoundException ce){
+			assertFalse(false);
+		}
+		catch(SQLException se){
+			assertFalse(false);
+		}
+		catch(Exception e){
+			assertFalse(false);
+		}
+	}
+	
+
+	/* @Test()
+	public void testAjouterSupprimerUtilisateur(){
+		boolean test=true;
+		
+		try{
+			BaseDeDonnees maBase = new BaseDeDonnees("jdbc:mysql://arthurguyader.ddns.net:3306/","admin", "admin");
+			maBase.ajouterNouvelUtilisateur("truc","truc", "projet",1);
+			// BaseDeDonnees maBase2 = new BaseDeDonnees("jdbc:oracle:thin:@localhost:1521:xe","admin", "admin");
+			maBase.supprimerUtilisateur("truc");
+			System.out.println("test ajout fait");
+		}
+		catch(Exception e){
+			System.out.println("test ajout échoué");
+			e.printStackTrace();
+			assertTrue(false);
+		}
+		finally {
+			maBase.deconnexion();
+		}
+	} */
+
+}
