21af798619448954fa966fe96fb0fa2bcaf3240d
Silmetal <Silmetal@users.noreply.github.com>
2017-06-23 13:13:25 +0200

    push

--- src/model/Attribut.java
+++ src/model/Attribut.java
@@ -11,20 +11,53 @@ import java.sql.*;
  * <P>- Si l'attribut est une clé primaire ou non
  * <P>- Si l'attribut est une clé étrangère ou non
  * <P>- Si c'est une clé étrangère, le nom de la table qui contient l'attribut qu'elle référence
- * <P>- * <P>- Si c'est une clé étrangère, le nom de l'attribut qu'elle référence
+ * <P>- Si c'est une clé étrangère, le nom de l'attribut qu'elle référence
  */
 public class Attribut {
 
-
+	/** 
+	 * Le nom de l'attribut
+	 */
 	private String nomVariable;
+	
+	/** 
+	 * Le type de l'attribut
+	 */
 	private Type type;
+	
+	/** 
+	 * La valeur associée au type de l'attribut (ex : INT(4))
+	 */
 	private int valeur;
+	
+	/** 
+	 * Le booléan indiquant si l'attribut peut être nul ou non
+	 */
 	private boolean estNonNul;
+	
+	/** 
+	 * Le booléan indiquant si l'attribut est une clé primaire ou non
+	 */
 	private boolean estClePrimaire;
+	
+	/** 
+	 * Le booléan indiquant si l'attribut référence une clé étrangère ou non
+	 */
 	private boolean aCleEtrangere;
+	
+	/** 
+	 * Le nom de la table contenant l'attribut référencé si l'attribut est une clé étrangère 
+	 */
 	private String referenceTableEtrangere;
+	
+	/** 
+	 * Le nom de l'attribut référencé par cet attribut
+	 */
 	private String referenceAttributEtranger;
 	
+	/** 
+	 *
+	 */
 	public Attribut(String nomVariable, Type type, int valeur, boolean estNonNul,  boolean estUnique, boolean estClePrimaire, boolean aCleEtrangere, String referenceTableEtrangere,String referenceAttributEtranger){
 		if(!aCleEtrangere){
 			referenceTableEtrangere="";
@@ -42,31 +75,55 @@ public class Attribut {
 		this.referenceTableEtrangere=referenceTableEtrangere;
 		this.referenceAttributEtranger=referenceAttributEtranger;
 	}
-
+	
+	/** 
+	 *
+	 */
 	public String getNomVariable(){
 		return this.nomVariable;
 	}
-
+	
+	/** 
+	 *
+	 */
 	public Type getType(){
 		return this.type;
 	}
-
+	
+	/** 
+	 * Retourne la valeur associée au type de l'attribut
+	 */
 	public int getValeur(){
 		return this.valeur;
 	}
-
+	
+	/** 
+	 * Retourne true si l'attribut peut être non nul, false sinon
+	 * @return true si l'attribut peut être non nul, false sinon
+	 */
 	public boolean getEstNonNul(){
 		return this.estNonNul;
 	}
-
+	
+	/** 
+	 * Retourne true si l'attribut est clé primaire, false sinon
+	 * @return true si l'attribut est clé primaire, false sinon
+	 */
 	public boolean getEstClePrimaire(){
 		return this.estClePrimaire;
 	}
-
+	
+	/** 
+	 * Retourne true si l'attribut est une clé étrangère, false sinon
+	 * @return true si l'attribut est une clé étrangère, false sinon
+	 */
 	public boolean getACleEtrangere(){
 		return this.aCleEtrangere;
 	}
-
+	
+	/** 
+	 * 
+	 */
 	public String getContrainte(){
 		String ret="";
 
@@ -78,10 +135,19 @@ public class Attribut {
 
 		return ret;
 	}
-
+	
+	/** 
+	 * Retourn le nom de la table contenant l'attribut référencé par cet attribut
+	 * @return le nom de la table contenant l'attribut référencé par cet attribut
+	 */
 	public String getReferenceTableEtrangere(){
 		return this.referenceTableEtrangere;
 	}
+	
+	/** 
+	 * Retourn le nom de l'attribut référencé par cet attribut
+	 * @return le nom de l'attribut référencé par cet attribut
+	 */
 	public String getReferenceAttributEtranger(){
 		return this.referenceAttributEtranger;
 	}
